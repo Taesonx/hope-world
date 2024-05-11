0a69178920dd3c7dcadace4594a2e98cf08230c9
Taesonx <168963622+Taesonx@users.noreply.github.com>
2024-05-11 18:55:44 +0300

    arson
    фамилии и так далее

---
+++ strings.cpp
@@ -0,0 +1,29 @@
+﻿// strings.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+
+    int  k, m, b, n, year, balls;
+    string surname = "", name = "";
+    int kol = 0;
+    cin >> k >> m >> b >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> surname >> name >> year >> balls;
+        if (year <= m and year >= k and balls == b) {
+            cout << surname << " " << name << endl;
+            kol++;
+        }
+    }
+    cout << kol;
+
+}
+
+
